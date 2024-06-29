02ab9bb71672268bae83a18b8bc8ec8b167e2971
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-06-29 12:05:52 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -18,18 +18,7 @@ import aiohttp
 from pyrogram.errors import RPCError, FloodWait
 import asyncio
 from main.ffmpeg import remove_all_tags, change_video_metadata, generate_sample_video, add_photo_attachment, merge_videos, unzip_file
-import pathlib
-import time
-from datetime import datetime
-import pyrogram
-from helper_funcs import gdriveTools
-from helper_funcs.bot_utils import sanitize_file_name, sanitize_text, get_readable_file_size
-from helper_funcs.display_progress import progress_for_pyrogram
-from plugins.gdriveupload import get_path_size
-from translation import Translation
 
-if bool(os.environ.get("WEBHOOK", False)):
-    from config import Config
 
 
 DOWNLOAD_LOCATION1 = "./screenshots"
@@ -1569,82 +1558,6 @@ async def set_photo(bot, msg):
         await msg.reply_text(f"Photo saved successfully as `{attachment_path}`.")
     except Exception as e:
         await msg.reply_text(f"Error saving photo: {e}")
-
-
-
-@Client.on_message(filters.command(["tleech"]))
-async def tg_to_gdrive_upload(client, update):
-    download_location = Config.DOWNLOAD_LOCATION + "/"
-    reply_message = await client.send_message(
-        chat_id=update.chat.id,
-        text=Translation.DOWNLOAD_START,
-        reply_to_message_id=update.message_id
-    )
-    c_time = time.time()
-    the_real_download_location = await client.download_media(
-        message=update.reply_to_message,
-        file_name=download_location,
-        progress=progress_for_pyrogram,
-        progress_args=(
-            Translation.DOWNLOAD_START,
-            reply_message,
-            c_time
-        )
-    )
-    if the_real_download_location is not None:
-        try:
-            await client.edit_message_text(
-                chat_id=update.chat.id,
-                message_id=reply_message.message_id,
-                text=Translation.SAVED_RECVD_DOC_FILE,
-            )
-        except:
-            pass
-
-    txt = update.text
-    if txt.find("rename") > -1 and len(txt[txt.find("rename") + 7:]) > 0:
-        custom_file_name = txt[txt.find("rename") + 7:]
-        custom_file_name = await sanitize_file_name(custom_file_name)
-        custom_file_name = await sanitize_text(custom_file_name)
-        new_file_name = download_location + custom_file_name
-        os.rename(the_real_download_location, new_file_name)
-        the_real_download_location = new_file_name
-
-    download_directory = the_real_download_location
-    if os.path.exists(download_directory):
-        up_name = pathlib.PurePath(download_directory).name
-        size = get_readable_file_size(get_path_size(download_directory))
-        try:
-            await client.edit_message_text(
-                chat_id=reply_message.chat.id,
-                message_id=reply_message.message_id,
-                text="Download Completed!!!\n Upload in progress",
-            )
-        except Exception as e:
-            pass
-
-        drive = gdriveTools.GoogleDriveHelper(up_name)
-        gd_url, index_url = drive.upload(download_directory)
-
-        button = []
-        button.append([pyrogram.types.InlineKeyboardButton(text="☁️ CloudUrl ☁️", url=f"{gd_url}")])
-
-        if Config.INDEX_URL:
-            button.append([pyrogram.types.InlineKeyboardButton(text="ℹ️ IndexUrl ℹ️", url=f"{index_url}")])
-
-        button_markup = pyrogram.types.InlineKeyboardMarkup(button)
-
-        await client.send_message(
-            chat_id=update.chat.id,
-            text=f"🤖: <b>{up_name}</b> has been Uploaded successfully to your Cloud🤒 \n📀 Size: {size}",
-            reply_to_message_id=update.message_id,
-            reply_markup=button_markup
-        )
-
-        if Config.INDEX_URL:
-            await generate_short_link(reply_message, index_url, up_name)
-
-        await reply_message.delete()
 
 
 
